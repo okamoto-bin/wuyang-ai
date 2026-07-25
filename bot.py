2d9921461fdc44238b6aab62ed4512d37522c1ee
Uryo <uryo4869@outlook.jp>
2026-07-26 01:58:10 +0900

    second

--- bot.py
+++ bot.py
@@ -4,13 +4,15 @@ import google.generativeai as genai
 import asyncio
 import time
 import os
-import random
 from dotenv import load_dotenv
+from pathlib import Path
 load_dotenv()
 
 # =====================
 # 🔐 環境変数
 # =====================
+env_path = Path(__file__).parent / ".env"
+load_dotenv(dotenv_path=env_path, override=True)
 DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 
